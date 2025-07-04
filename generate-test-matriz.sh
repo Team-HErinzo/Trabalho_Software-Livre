a9d1f4bedc1b8b4899bcc173dadec196242b08b0
Eric Andrew <ericpretito120@gmail.com>
2025-07-04 02:21:02 -0300

    Adicionando o gerador do programa matriz

---
+++ generate-test-matriz.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Gera duas matrizes 2x2 e a operação 'a' (adição) como exemplo
+cat <<EOF > test1-matriz.in
+2
+1 2
+3 4
+5 6
+7 8
+a
+EOF
+echo "Arquivo test1-matriz.in gerado com sucesso!"
